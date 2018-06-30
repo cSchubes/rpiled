e54a8bc490e864f7b1360a6d403643ad120e5d00
alanzeng17 <alanzeng17@gmail.com>
2018-06-30 10:32:41 -0500

    temp hcanges

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 -Updating input fields
     -Map Button to JS
     -Change Color wheel on RGB Sliders
+        -Set slider value to default, update them on change
 
 -Implement new page to create "preset colors changes"
     -Add colors to "array", set time interval(static or dynamic?)
--- public/javascripts/index_scripts.js
+++ public/javascripts/index_scripts.js
@@ -1,4 +1,36 @@
+// update color wheel based on hex input
 document.getElementById('hex-btn').onclick = () => {
     console.log($('#event-color').val())
     $('#color-input').wheelColorPicker('setValue', $('#event-color').val());
 };
+
+// slider move update function
+$(function() {
+    $('#color-input').on('slidermove', function() {
+        $('#event-color').val($("#color-input").wheelColorPicker('getValue', 'hex'));
+        var rgb = $("#color-input").wheelColorPicker('getValue', 'rgb');
+        var arr = rgb.split(" ")
+        $('#R-color').val(arr[0]);
+        $('#G-color').val(arr[1]);
+        $('#B-color').val(arr[2]);
+        $('#r-slider').val(arr[0]);
+        $('#g-slider').val(arr[1]);
+        $('#b-slider').val(arr[2]);
+    });
+});
+var rSlider = document.getElementById('r-slider');
+var rInput = document.getElementById("R-color");
+var gSlider = document.getElementById('g-slider');
+var gInput = document.getElementById("G-color");
+var bSlider = document.getElementById('b-slider');
+var bInput = document.getElementById("B-color");
+
+rSlider.onchange = function(){
+    rInput.value = rSlider.value;
+    var co = [rInput, bInput, gInput, 1]
+    $('#color-input').wheelColorPicker('setValue', co);
+}
+rInput.onchange = function() {
+    rSlider.value = rInput.value;
+    //$('#color-input').wheelColorPicker('setValue', $('#event-color').val());
+}
