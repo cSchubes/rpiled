460df2070e23062bd45adf29e859deb5f11a786c
Carson Schubert <carson.schubert14@gmail.com>
2018-07-12 20:30:06 -0500

    resin: test multiple aniamtions

--- ledController/animations.js
+++ ledController/animations.js
@@ -6,7 +6,6 @@ exports.rainbow = (req, res, next) => {
     console.log('Starting rainbow animations.');
     if (globals.CURR_ANIMATION_PID != -1) {
         process.kill(globals.CURR_ANIMATION_PID, 'SIGINT');
-        process.kill(globals.CURR_ANIMATION_PID+1, 'SIGINT');
         console.log('killed the old process' + globals.CURR_ANIMATION_PID);
     }
     let child = execFile('python', [`${__dirname}/animations/rainbow.py`], (err, stdout, stderr) => {
@@ -27,7 +26,6 @@ exports.strandtest = (req, res, next) => {
     console.log('Starting strand test.');
     if (globals.CURR_ANIMATION_PID != -1) {
         process.kill(globals.CURR_ANIMATION_PID, 'SIGINT');
-        process.kill(globals.CURR_ANIMATION_PID+1, 'SIGINT');
         console.log('killed the old process');
     }
     let child = execFile('python', [`${__dirname}/animations/strandtest.py`], (err, stdout, stderr) => {
