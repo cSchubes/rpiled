8ccfe15705e2bdc867110a3c10a610a4c81c9f84
Carson Schubert <carson.schubert14@gmail.com>
2018-07-12 20:27:41 -0500

    resin: test multiple aniamtions

--- ledController/animations.js
+++ ledController/animations.js
@@ -1,14 +1,15 @@
 // animation handlers for the router
-const { exec } = require('child_process');
+const { execFile } = require('child_process');
 var globals = require('./globals');
 
 exports.rainbow = (req, res, next) => {
     console.log('Starting rainbow animations.');
     if (globals.CURR_ANIMATION_PID != -1) {
         process.kill(globals.CURR_ANIMATION_PID, 'SIGINT');
+        process.kill(globals.CURR_ANIMATION_PID+1, 'SIGINT');
         console.log('killed the old process' + globals.CURR_ANIMATION_PID);
     }
-    let child = exec(`python ${__dirname}/animations/rainbow.py`, (err, stdout, stderr) => {
+    let child = execFile('python', [`${__dirname}/animations/rainbow.py`], (err, stdout, stderr) => {
         console.log(err);
         console.log(stdout);
         console.log(stderr);
@@ -26,9 +27,10 @@ exports.strandtest = (req, res, next) => {
     console.log('Starting strand test.');
     if (globals.CURR_ANIMATION_PID != -1) {
         process.kill(globals.CURR_ANIMATION_PID, 'SIGINT');
+        process.kill(globals.CURR_ANIMATION_PID+1, 'SIGINT');
         console.log('killed the old process');
     }
-    let child = exec(`python ${__dirname}/animations/strandtest.py`, (err, stdout, stderr) => {
+    let child = execFile('python', [`${__dirname}/animations/strandtest.py`], (err, stdout, stderr) => {
         console.log(err);
         console.log(stdout);
         console.log(stderr);
