b727f92cc5c982d5731994f007082519528fba2e
Carson Schubert <carson.schubert14@gmail.com>
2018-07-13 19:07:43 -0500

    pls

--- ledController/test.js
+++ ledController/test.js
@@ -1,5 +1,11 @@
-let constants = require('./constants');
+let constants = require('./globals');
+const { execFile } = require('child_process');
 
 console.log(constants.CURR_ANIMATION_PID);
-constants.CURR_ANIMATION_PID = 10;
+let child = execFile('python', [`${__dirname}/animations/rainbow.py`], (err, stdout, stderr) => {
+    console.log(err);
+    console.log(stdout);
+    console.log(stderr);
+});
+constants.CURR_ANIMATION_PID = child.pid;
 console.log(constants.CURR_ANIMATION_PID);
