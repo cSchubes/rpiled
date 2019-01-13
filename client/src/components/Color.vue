3cd3f9496ee60087396243d0bb85cc1bfcfcf0fe
alanzeng17 <alanzeng17@gmail.com>
2019-01-12 21:45:53 -0600

    optional brightness slider added

--- client/src/components/Color.vue
+++ client/src/components/Color.vue
@@ -3,54 +3,66 @@
     <v-layout
       text-xs-center
       wrap
+      column
     >
       <v-flex mb-4>
-        <verte display='widget' value='#fef8e2' picker='wheel' model='hex' :rgbSliders="true" :recentColors='null' v-model="cVal"></verte>
-        <v-layout column class="light--text">
-            <v-flex xs4><button>Toggle Range</button></v-flex>
-            <v-flex xs4 v-if="rangeButton == false">
+        <verte display='widget' value='#fef8e2' picker='wheel' model='hex' :rgbSliders="true" :recentColors='null' :enableAlpha='false' v-model="cVal"></verte>
+      </v-flex>
+      <v-flex>
+            <button @click='rangeToggle()'>Toggle Brightness Range</button>
+            <v-layout align-center justify-center row fill-height v-if="rangeButton == true">
+                <v-flex shrink style="width: 40px">   
+                    <v-text-field
+                        v-model="price[0]"
+                        class="mt-0"
+                        hide-details
+                        single-line
+                        type="number"
+                        style="position: relative; left:20em;"
+                    ></v-text-field>
+                </v-flex>
+    
+                <v-flex>
+                    <v-range-slider
+                        v-model="price"
+                        style="width: 400px; position: relative; left: 21em; top: .8em;"
+                        :max="100"
+                        :min="0"
+                        :step="1"
+                    ></v-range-slider>
+                </v-flex>
+    
                 <v-flex
-          shrink
-          style="width: 60px"
-        >
-          <v-text-field
-            v-model="price[0]"
-            class="mt-0"
-            hide-details
-            single-line
-            type="number"
-          ></v-text-field>
-        </v-flex>
-  
-        <v-flex>
-          <v-range-slider
-            v-model="price"
-            maxWidth='100px'
-            :max="100"
-            :min="0"
-            :step="1"
-          ></v-range-slider>
-        </v-flex>
-  
-        <v-flex
-          shrink
-          style="width: 60px"
-        >
-          <v-text-field
-            v-model="price[1]"
-            class="mt-0"
-            hide-details
-            single-line
-            type="number"
-          ></v-text-field>
-        </v-flex>
-            </v-flex>
-            <v-flex xs4 v-else>
-                <v-slider v-model="slider" label="Label"></v-slider>
-            </v-flex>
-        </v-layout>
+                shrink
+                style="width: 40px"
+                >
+                    <v-text-field
+                        v-model="price[1]"
+                        class="mt-0"
+                        hide-details
+                        single-line
+                        type="number"
+                        style="position: relative; right:18em;"
+                    ></v-text-field>
+                </v-flex>
+            </v-layout >
+            <v-layout align-center justify-center row fill-height v-else style="position: relative; left: 28em">
+                <v-flex shrink style="width: 60px">   
+                    <v-text-field
+                        v-model="slider"
+                        class="mt-0"
+                        hide-details
+                        single-line
+                        style="width: 30px; position: relative; left: 1.7em; top: -.5em"
+                        type="number"
+                    ></v-text-field>
+                </v-flex>
+                <v-flex>
+                    <v-slider v-model="slider" style='width: 250px'></v-slider>
+                </v-flex>
+            </v-layout>
         <br>
-        <v-btn large=true @click='submit("hi")' v-bind:style="{backgroundColor: cVal}">Submit</v-btn>
+        <v-btn @click='submit("hi")' v-bind:style="{backgroundColor: cVal}">Submit</v-btn>
       </v-flex>
     </v-layout>
   </v-container>
@@ -76,6 +88,9 @@ export default {
         },
         change: function() {
             console.log(this.cVal)
+        },
+        rangeToggle: function() {
+            this.rangeButton = !this.rangeButton;
         }
     }
 }
